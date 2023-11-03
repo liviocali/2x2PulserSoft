056e990b4eab4615e93d09cf4cf1674c154084e5
liviocali <liviocalivers@hotmail.com>
2023-11-03 13:02:51 -0500

    Add period as argument for trigger

--- trigger.py
+++ trigger.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python
+from signal import signal, SIGINT
+from sys import exit
+import argparse
 
 import time
 
 import pigpio
 
-GPIO=13
+GPIO=13\
 
-square = []
+parser = argparse.ArgumentParser(description='2x2 Pulser trigger script')
+parser.add_argument('-p','--period',help='Period [ms]',action='store_true',default=10)
+args = parser.parse_args()
 
-#                          ON       OFF    MICROS
-square.append(pigpio.pulse(1<<GPIO, 0,       10))
-square.append(pigpio.pulse(0,       1<<GPIO, 9999))
+def ctrlc_handler(signal_received, frame):
+    pi.wave_tx_stop()
+    pi.wave_delete(wid)
+    pi.stop()
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    exit(0)
 
-pi = pigpio.pi() # connect to local Pi
+if __name__=='__main__':
+   signal(SIGINT, ctrlc_handler)
+   square = []
+   period_us = int(args.period*1000)
 
-pi.set_mode(GPIO, pigpio.OUTPUT)
+   #                          ON       OFF    MICROS
+   square.append(pigpio.pulse(1<<GPIO, 0,       10))
+   square.append(pigpio.pulse(0,       1<<GPIO, 10000))
 
-pi.wave_add_generic(square)
+   pi = pigpio.pi() # connect to local Pi
 
-wid = pi.wave_create()
+   pi.set_mode(GPIO, pigpio.OUTPUT)
 
-if wid >= 0:
-   pi.wave_send_repeat(wid)
-   time.sleep(60)
-   pi.wave_tx_stop()
-   pi.wave_delete(wid)
+   pi.wave_add_generic(square)
 
-pi.stop()
+   wid = pi.wave_create()
+
+   if wid >= 0:
+      pi.wave_send_repeat(wid)
+      time.sleep(60)
+      pi.wave_tx_stop()
+      pi.wave_delete(wid)
+
+   pi.stop()
